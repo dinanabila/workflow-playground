1bc5111679072be042d3d9ef1486cee4b5e1505e
dinanabila <dinanabb@gmail.com>
2026-07-21 21:11:33 +0700

    penyesuaian pra tabrakan II

---
+++ total_days.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+df = pd.read_csv('https://raw.githubusercontent.com/dinanabila/workflow-playground/refs/heads/main/source_data.csv')
+# df_result = pd.DataFrame({'revenue_total': [df['revenue'].sum()]})
+
+# df_result.to_csv('result_data.csv', index=False)
